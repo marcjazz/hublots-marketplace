c1569c9712c88eb7d4d0fc7f9c311d8728eeaab3
Marco Kuidja <kuidjamarco@gmail.com>
2025-12-25 17:27:04 +0100

    feat(terraform): update ghcr mirror permissions and remove lifecycle ignore

--- terraform/modules/ghcr-mirror/main.tf
+++ terraform/modules/ghcr-mirror/main.tf
@@ -25,13 +25,9 @@ resource "google_artifact_registry_repository" "ghcr_io_mirror" {
       tag_state  = "UNTAGGED"
     }
   }
-
-  lifecycle {
-    ignore_changes = [remote_repository_config[0].upstream_credentials]
-  }
 }
 
-resource "google_artifact_registry_repository_iam_member" "ghcr_io_mirror_reader" {
+resource "google_artifact_registry_repository_iam_member" "ghcr_io_mirror_writer" {
   for_each   = var.service_accounts
   repository = google_artifact_registry_repository.ghcr_io_mirror.name
   location   = var.region
