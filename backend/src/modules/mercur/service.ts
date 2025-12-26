32d0b6cae0d9a89322d616642136d0516d98fdbf
Marco Kuidja <kuidjamarco@gmail.com>
2025-12-26 23:52:54 +0100

    feat: implement mercur marketplace orchestration module

---
+++ backend/src/modules/mercur/service.ts
@@ -0,0 +1,8 @@
+import { MedusaService } from "@medusajs/framework/utils"
+import { ChatMessage } from "./models"
+
+export default class MercurService extends MedusaService({
+  ChatMessage,
+}) {
+  // Common MercurJS orchestration logic
+}
