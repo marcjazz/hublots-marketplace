2a4f8dac7030244c1529f20821709293dbe73e44
Marco Kuidja <kuidjamarco@gmail.com>
2026-01-01 21:55:50 +0100

    refactor(storefront): adopt hybrid type strategy with @medusajs/types and @medusajs/client-types

--- storefront/src/types/medusa.ts
+++ storefront/src/types/medusa.ts
@@ -1,70 +1,59 @@
-import { 
-  StoreProductListResponse as MedusaStoreProductListResponse, 
-  StoreProductParams as MedusaStoreProductParams, 
-  StoreRegion as MedusaStoreRegion,
-  StoreCart as MedusaStoreCart,
-  StoreOrder as MedusaStoreOrder,
-  StoreCustomer as MedusaStoreCustomer,
-  StoreProductCategory as MedusaStoreProductCategory,
-  StoreCartResponse as MedusaStoreCartResponse,
-  StoreOrderResponse as MedusaStoreOrderResponse,
-  StoreCustomerResponse as MedusaStoreCustomerResponse,
-  StoreProductCategoryListResponse as MedusaStoreProductCategoryListResponse,
-  StoreCollectionListResponse as MedusaStoreCollectionListResponse,
-  StoreCartLineItem as MedusaStoreCartLineItem,
-  StoreOrderLineItem as MedusaStoreOrderLineItem,
-  StoreCartShippingMethod as MedusaStoreCartShippingMethod,
-  StoreCartShippingOption as MedusaStoreCartShippingOption,
-  StorePaymentSession as MedusaStorePaymentSession,
-  StoreProductOption as MedusaStoreProductOption,
-  StoreProductOptionValue as MedusaStoreProductOptionValue,
-  StoreProductTag as MedusaStoreProductTag,
-  StoreReturnReason as MedusaStoreReturnReason,
-  StoreUpdateCart as MedusaStoreUpdateCart,
-  StoreUpdateCustomer as MedusaStoreUpdateCustomer,
-  StoreUpdateCustomerAddress as MedusaStoreUpdateCustomerAddress,
-} from '@medusajs/types'
+import type {
+  Region,
+  ProductVariant,
+  Cart,
+  Order,
+  Customer,
+  ProductCategory,
+  ProductCollection,
+  StoreGetProductsParams,
+  StoreProductsListRes,
+} from "@medusajs/client-types"
+
+import { HttpTypes as MedusaHttpTypes } from "@medusajs/types"
 
 export namespace HttpTypes {
-  export type StoreProduct = MedusaStoreProductListResponse["products"][0]
-  export type StoreCart = MedusaStoreCart & { promotions?: any[] }
-  export type StoreOrder = MedusaStoreOrder
-  export type StoreRegion = MedusaStoreRegion
-  export type StoreCustomer = MedusaStoreCustomer
-  export type StoreProductCategory = MedusaStoreProductCategory
-  export type StoreCollection = MedusaStoreCollectionListResponse["collections"][0]
-  export type StoreVariant = NonNullable<StoreProduct["variants"]>[0]
-  export type StoreProductVariant = StoreVariant
-  export type StoreProductParams = MedusaStoreProductParams
+  export type StoreProduct = MedusaHttpTypes.StoreProduct
+  export type StoreCart = MedusaHttpTypes.StoreCart & { promotions?: any[] }
+  export type StoreOrder = MedusaHttpTypes.StoreOrder
+  export type StoreRegion = MedusaHttpTypes.StoreRegion
+  export type StoreCustomer = MedusaHttpTypes.StoreCustomer
+  export type StoreProductCategory = MedusaHttpTypes.StoreProductCategory
+  export type StoreCollection = MedusaHttpTypes.StoreCollection
+  export type StoreVariant = MedusaHttpTypes.StoreProductVariant
+  export type StoreProductVariant = MedusaHttpTypes.StoreProductVariant
+  export type StoreProductParams = MedusaHttpTypes.StoreProductParams
   export type FindParams = any
-  export type StoreCartResponse = MedusaStoreCartResponse
-  export type StoreOrderResponse = MedusaStoreOrderResponse
-  export type StoreCustomerResponse = MedusaStoreCustomerResponse
-  export type StoreProductCategoryListResponse = MedusaStoreProductCategoryListResponse
-  export type StoreCollectionListResponse = MedusaStoreCollectionListResponse
-  export type StoreCartAddress = any // MedusaStoreAddress was missing
-  export type StoreCartLineItem = MedusaStoreCartLineItem
-  export type StoreOrderLineItem = MedusaStoreOrderLineItem
-  export type StoreCartShippingMethod = MedusaStoreCartShippingMethod
-  export type StoreCartShippingOption = MedusaStoreCartShippingOption
-  export type StoreCustomerAddress = any // MedusaStoreAddress was missing
-  export type StorePaymentSession = MedusaStorePaymentSession
-  export type StoreProductOption = MedusaStoreProductOption
-  export type StoreProductOptionValue = MedusaStoreProductOptionValue
-  export type StoreProductTag = MedusaStoreProductTag
-  export type StoreReturnReason = MedusaStoreReturnReason
-  export type StoreUpdateCart = MedusaStoreUpdateCart
-  export type StoreUpdateCustomer = MedusaStoreUpdateCustomer
-  export type StoreUpdateCustomerAddress = MedusaStoreUpdateCustomerAddress
+  export type StoreCartResponse = MedusaHttpTypes.StoreCartResponse
+  export type StoreOrderResponse = MedusaHttpTypes.StoreOrderResponse
+  export type StoreCustomerResponse = MedusaHttpTypes.StoreCustomerResponse
+  export type StoreProductCategoryListResponse = MedusaHttpTypes.StoreProductCategoryListResponse
+  export type StoreCollectionListResponse = MedusaHttpTypes.StoreCollectionListResponse
+  export type StoreCartAddress = MedusaHttpTypes.StoreCartAddress
+  export type StoreCartLineItem = MedusaHttpTypes.StoreCartLineItem
+  export type StoreOrderLineItem = MedusaHttpTypes.StoreOrderLineItem
+  export type StoreCartShippingMethod = MedusaHttpTypes.StoreCartShippingMethod
+  export type StoreCartShippingOption = MedusaHttpTypes.StoreCartShippingOption
+  export type StoreCustomerAddress = MedusaHttpTypes.StoreCustomerAddress
+  export type StorePaymentSession = MedusaHttpTypes.StorePaymentSession
+  export type StoreProductOption = MedusaHttpTypes.StoreProductOption
+  export type StoreProductOptionValue = MedusaHttpTypes.StoreProductOptionValue
+  export type StoreProductTag = MedusaHttpTypes.StoreProductTag
+  export type StoreReturnReason = MedusaHttpTypes.StoreReturnReason
+  export type StoreUpdateCart = MedusaHttpTypes.StoreUpdateCart
+  export type StoreUpdateCustomer = MedusaHttpTypes.StoreUpdateCustomer
+  export type StoreUpdateCustomerAddress = MedusaHttpTypes.StoreUpdateCustomerAddress
   export type StorePaymentProviderListResponse = any
 }
 
 export type {
-  MedusaStoreProductListResponse as StoreProductListResponse,
-  MedusaStoreProductParams as StoreProductParams,
-  MedusaStoreRegion as StoreRegion,
-  MedusaStoreCart as StoreCart,
-  MedusaStoreOrder as StoreOrder,
-  MedusaStoreCustomer as StoreCustomer,
-  MedusaStoreProductCategory as StoreProductCategory
+  StoreProductsListRes,
+  StoreGetProductsParams,
+  Region,
+  ProductVariant,
+  Cart,
+  Order,
+  Customer,
+  ProductCategory,
+  ProductCollection,
 }
